bc6b8a41a76e45ff9e0e9bb08fa899fe5180f4e6
caspianroleplay <dtxresmi@gmail.com>
2026-03-01 17:17:35 +0400

    ModeScreen.tsx

--- src/screens/ModeScreen.tsx
+++ src/screens/ModeScreen.tsx
@@ -20,19 +20,19 @@ export const ModeScreen = React.memo(({ navigation }: InitiationScreenType) => {
 
   return (
     <LoaderContainer>
-      <Text style={styles.titleSub}>Выбор карты</Text>
+      <Text style={styles.titleSub}>Xəritə seçin</Text>
       <View style={styles.body}>
         <ModeItem
           id={0}
           onPress={onPress}
-          text="Стандартная карта"
+          text="Standart Xəritə"
           image={Images.Default}
           color={['#a4956e99', '#f5422a60']}
         />
         <ModeItem
           id={1}
           onPress={onPress}
-          text="Зимняя карта"
+          text="Qış Xəritəsi"
           image={Images.Snow}
           color={['#88effc99', '#6e2af560']}
         />
